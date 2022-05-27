471488f96f28b7cdece6a6797fde6d83b881ef45
Tanjir Ahmed <tanjira584@gmail.com>
2022-05-27 10:10:04 +0600

    Admin role handle

--- index.js
+++ index.js
@@ -18,7 +18,22 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+/*------------Verify JWT <Token------*/
+function verifyJwt(req, res, next) {
+    const bearToken = req.headers.authorization;
+    if (!bearToken) {
+        return res.status(401).send({ message: "Unauthorize Access" });
+    }
 
+    const token = bearToken.split(" ")[1];
+    jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 async function run() {
     try {
         await client.connect();
@@ -32,6 +47,18 @@ async function run() {
             .db("jantrik-app")
             .collection("payment");
 
+        /*-------Verify Admin-------*/
+        async function verifyAdmin(req, res, next) {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({
+                email: requester,
+            });
+            if (requesterAccount.role === "admin") {
+                next();
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+        }
         /*--------Get Client Secret Key-----*/
         app.post("/client-payment-intent", async (req, res) => {
             const service = req.body;
@@ -54,7 +81,7 @@ async function run() {
         });
 
         /*-------All Order Get Controller----*/
-        app.get("/orders", async (req, res) => {
+        app.get("/orders", verifyJwt, async (req, res) => {
             const query = {};
             const orders = await orderCollection.find(query).toArray();
             res.send(orders);
@@ -70,36 +97,73 @@ async function run() {
         app.patch("/order/:id", async (req, res) => {
             const id = req.params.id;
             const payment = req.body;
+            console.log(payment);
             const filter = { _id: ObjectId(id) };
             const updateDoc = {
                 $set: {
-                    paid: true,
-                    transactionId: payment.transactionId,
+                    ...payment,
                 },
             };
             const paid = await paymentCollection.insertOne(payment);
             const result = await orderCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
+        /*---------Order Delete Controller-----*/
+        app.delete("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        /*--------All Product Get Controller------*/
         app.get("/products", async (req, res) => {
             const query = {};
             const products = await productCollection.find(query).toArray();
-            res.send(products);
+            res.send(products.reverse());
         });
 
-        /*-----Add New Post-------*/
+        /*-----Single Product Post Controller-------*/
         app.post("/products", async (req, res) => {
             const product = req.body;
             const result = await productCollection.insertOne(product);
             res.send(result);
         });
+        /*--------Single Product Get Controller------*/
         app.get("/product/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await productCollection.findOne(query);
             res.send(result);
         });
+        /*--------Single Product PATCH Controller------*/
+        app.patch("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const newStock = req.body.stock;
+            const prod = await productCollection.findOne({ _id: ObjectId(id) });
+            const updateDoc = {
+                $set: {
+                    ...prod,
+                    stock: newStock,
+                },
+            };
+            const result = await productCollection.updateOne(filter, updateDoc);
 
+            res.send(result);
+        });
+        /*---------Product Delete Controller------*/
+        app.delete("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+        /*-------All Review Get Controoler----*/
+        app.get("/reviews", async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result.reverse());
+        });
         /*-----Single Review Post Controller-----*/
         app.post("/reviews", async (req, res) => {
             const review = req.body;
@@ -113,6 +177,28 @@ async function run() {
             const result = await userCollection.find(query).toArray();
             res.send(result);
         });
+        /*-----------Single User Patch Controller-------*/
+        app.put(
+            "/user/admin/:email",
+            verifyJwt,
+            verifyAdmin,
+            async (req, res) => {
+                const email = req.params.email;
+
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: {
+                        role: "admin",
+                    },
+                };
+
+                const result = await userCollection.updateOne(
+                    filter,
+                    updateDoc
+                );
+                res.send(result);
+            }
+        );
         /*-------Create User Post Controller-------*/
         app.put("/user/:email", async (req, res) => {
             const email = req.params.email;
@@ -131,6 +217,15 @@ async function run() {
                 expiresIn: "1d",
             });
             res.send({ result, token });
+        });
+        /*---------Get The Admin User ------*/
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user?.role === "admin";
+
+            res.send({ admin: isAdmin });
         });
     } finally {
     }
